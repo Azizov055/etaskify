09e61e913f7b26d26b8ac1b66cbd3ac9269f445e
Admin <admin@Ismail-MacBook-Pro.local>
2020-10-23 12:16:55 +0400

    Added validation error handler

---
+++ src/main/java/az/ibar/etaskify/response/ValidationResponse.java
@@ -0,0 +1,15 @@
+package az.ibar.etaskify.response;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class ValidationResponse extends ApiResponse {
+    private List<String> errors;
+
+    public ValidationResponse(List<String> errors) {
+        super(false, "Validation error");
+        this.errors = errors;
+    }
+}
