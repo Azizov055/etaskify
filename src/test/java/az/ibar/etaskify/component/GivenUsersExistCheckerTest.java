74739ecb1f52fa27fd701a7d71da8826861aa71e
Ismail <ismail@Ismails-MacBook-Pro.local>
2020-10-28 22:33:50 +0400

    added unit test to given user exist checker

--- src/test/java/az/ibar/etaskify/component/GivenUsersExistCheckerTest.java
+++ src/test/java/az/ibar/etaskify/component/GivenUsersExistCheckerTest.java
@@ -1,6 +1,12 @@
 package az.ibar.etaskify.component;
 
+import az.ibar.etaskify.exception.ResourceNotFoundException;
+import az.ibar.etaskify.model.User;
 import az.ibar.etaskify.repository.UserRepository;
+import javassist.NotFoundException;
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatchers;
@@ -9,6 +15,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,9 +31,24 @@ class GivenUsersExistCheckerTest {
     @MockBean
     private UserRepository userRepository;
 
+    private List<Long> userIds = new ArrayList<>();
+
+    @BeforeEach
+    void setUp() {
+        userIds.add(1L);
+        userIds.add(2L);
+        userIds.add(3L);
+    }
+
     @Test
     void users_exist() {
         Mockito.doReturn(true).when(userRepository).existsById(ArgumentMatchers.anyLong());
-        givenUsersExistChecker.check(ArgumentMatchers.anyList());
+        givenUsersExistChecker.check(userIds);
+    }
+
+    @Test
+    void user_not_exist() {
+        Mockito.doReturn(false).when(userRepository).existsById(ArgumentMatchers.anyLong());
+        Assert.assertThrows(ResourceNotFoundException.class, () -> givenUsersExistChecker.check(userIds));
     }
 }
